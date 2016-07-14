03a8f51a1f6aee33f79c2b3fc17b79ce7c4c3962
KemalC <ddeveloperr@gmail.com>
2016-07-14 21:40:15 +0000

    Done with Updating Article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,44 @@
 class ArticlesController < ApplicationController
-    def new
+    
+    def index
+      @articles = Article.all
     end
+    
+    def show
+      @article = Article.find(params[:id])    
+    end
+    
+    def new
+      @article = Article.new    
+    end
+    
+    def edit
+      @article = Article.find(params[:id])
+    end
+    
+    def create
+      @article = Article.new(articles_params)
+    
+      if @article.save
+      redirect_to @article
+      else 
+        render 'new'
+      end
+    end
+    
+    def update
+      @article = Article.find(params[:id])
+    
+      if @article.update(articles_params)
+        redirect_to @article
+      else
+        render 'edit'
+      end
+    end
+    
+    private
+    def articles_params
+      params.require(:article).permit(:title, :text)
+    end
+    
 end
