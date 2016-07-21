66c030c93fad5b35a7ab5bdbac584c17ced8f43e
KemalC <ddeveloperr@gmail.com>
2016-07-21 02:07:23 +0000

    Delete comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,6 +5,17 @@ class CommentsController < ApplicationController
       redirect_to articles_path(@article)
     end
     
+    def destroy
+      # find the correct article
+      @article = Article.find(params[:article_id])
+      # locate the right comment within
+      @comment = @article.comments.find(params[:id])
+      # remove it from db
+      @comment.destroy
+      # back to the show action for the article
+      redirect_to article_path(@article)
+    end
+    
     private
       def comment_params
         params.require(:comment).permit(:commenter, :body)  
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,6 @@
 class Article < ActiveRecord::Base
-  has_many :comments
+  # add dependent: :destroy  ==>> when we delete an article, its associated comments will also need to be deleted
+  has_many :comments, dependent: :destroy  
   validates :title, presence: true,
                     length: { minimum: 5}
 end
